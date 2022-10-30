e5f751cc8c746afa7e5f7db657f79250ab169acd
hauretax <huggotrico@protonmail.ch>
2022-10-30 11:22:30 +0100

    send one scream

--- www/assets/chart.js
+++ www/assets/chart.js
@@ -1,17 +1,13 @@
-const data = {
-    labels: labels,
-    datasets: [
-      {
-        label: 'Dataset 1',
-        data: Utils.numbers(NUMBER_CFG),
-        borderColor: Utils.CHART_COLORS.red,
-        backgroundColor: Utils.transparentize(Utils.CHART_COLORS.red, 0.5),
-      },
-      {
-        label: 'Dataset 2',
-        data: Utils.numbers(NUMBER_CFG),
-        borderColor: Utils.CHART_COLORS.blue,
-        backgroundColor: Utils.transparentize(Utils.CHART_COLORS.blue, 0.5),
-      }
-    ]
-  };
+var xValues = [50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150];
+var yValues = [7, 8, 8, 9, 9, 9, 10, 11, 14, 14, 15];
+
+new Chart("edaChart", {
+    type: "line",
+    data: {
+        datasets: [{
+            backgroundColor: "rgba(255,255,255,0.50)",
+            borderColor: "#33EE88",
+            data: yValues
+        }]
+    },
+});
